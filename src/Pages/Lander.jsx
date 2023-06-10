484a02232b9528bb7907f530e324460437c6ac7f
Nikhil <nikhilarora10b@gmail.com>
2023-06-11 01:16:39 +0530

    Added Lander Page and tailwind css

---
+++ src/Pages/Lander.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Lander = () => {
+    return (
+        <div className=" m-3 Heading font-roboto text-2xl sm:text-3xl md:text-5xl font-bold text-white text-center py-10">
+            HACK UNICORN
+          </div>
+        );
+    };
+    
+    export default Lander;
+    
