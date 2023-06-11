e87fcd88dbc1973bf32606cd4cc681cd73714d1f
avikapoor24 <avikapoor244@gmail.com>
2023-06-11 16:33:11 +0530

    Navbar without icons
    Signed-off-by: avikapoor24 <avikapoor244@gmail.com>

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Navbar.css";
+import
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          
+          <h2>
+           FINOVATECH.IO
+          </h2>
+        </div>
+
+        {/* 2nd menu part  */}
+        <div className={"menu-link"}>
+          <ul>
+            <li>
+              <NavLink to="/">Services</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Portfolio</NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">Vision</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">About Us</NavLink>
+            </li>
+            <li>           
+              <NavLink to="/#" className="logIn">Log In</NavLink>
+            </li>
+            <li>
+            
+              <button className="navButton">Get Started</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    </>
+  );
+};
+
+export default Navbar;
