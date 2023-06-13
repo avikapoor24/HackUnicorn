0729a4cc7676c1abcaaead8437f9b355216391c7
Nikhil <nikhilarora10b@gmail.com>
2023-06-14 01:36:07 +0530

    created service page

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import image from "../Assets/Images/i.jpeg"
+import i from "../Assets/Images/n.png"
+import im from "../Assets/Images/o.png"
+import ima from "../Assets/Images/p.png"
 const Services = () => {
   return (
-    <div className=" m-3 Heading font-roboto text-2xl sm:text-3xl md:text-5xl font-bold text-white text-center py-10">
-      SERVICES
-    </div>
+    <div
+    className="h-[50vh] bg-center bg-cover bg-no-repeat"
+    style={{
+      backgroundImage: `url(${image})`,
+    }}
+  >
+    <div className="h-full flex flex-col justify-start pt-20 px-4 ">
+        <h1 className="text-8xl  text-white px-40">Dive Into the New Age of  </h1>
+<h1 className="text-8xl  text-white px-40"> Accelerated Analytics
+</h1>
+
+
+<h2 className="text-5xl  text-white px-40 pt-11">Our Services</h2>
+      </div>
+      <div
+          className="h-[60vh] bg-center bg-cover bg-no-repeat"
+          style={{
+              backgroundImage: `url(${im})`,
+          }}
+      >
+
+          </div>
+          <div
+          className="h-[60vh]  bg-center bg-cover bg-no-repeat"
+          style={{
+              backgroundImage: `url(${ima})`,
+          }}
+      >
+
+          </div>
+          <div
+          className="h-[60vh] bg-center bg-cover bg-no-repeat"
+          style={{
+              backgroundImage: `url(${i})`,
+          }}
+      >
+
+          </div>
+      
+      
+
+  </div>
+  
+  
   );
 };
 
 export default Services;
+
+
