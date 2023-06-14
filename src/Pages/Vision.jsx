2640e56ac79e545e6e007725eb842d8116b1f743
avikapoor24 <avikapoor244@gmail.com>
2023-06-14 15:55:58 +0530

    VISION CSS
    Signed-off-by: avikapoor24 <avikapoor244@gmail.com>

--- src/Pages/Vision.jsx
+++ src/Pages/Vision.jsx
@@ -5,7 +5,7 @@ import image2 from "../Assets/Images/bgimgVision.png"
 const Vision = () => {
   return (
     <>
-    <div className="h-[85vh] bg-center bg-cover bg-no-repeat mb-[25%] "
+    <div className="h-[85vh] bg-center bg-cover bg-no-repeat mb-[30%] "
     style={{
       backgroundImage: `url(${image})`,
     }}>
@@ -19,29 +19,39 @@ const Vision = () => {
        border-[0.2px] border-[#858585] 
        mx-[5%] mt-[15%] 
        ">
-      <div>
-          
-          </div>
           <div style={{
       backgroundImage: `url(${image})`,
     }}>
             
-    </div>
-
-          <div className="text-white text-3xl w-[300px] relative top-[50px] left-[60%]">
-            <p>
-In the vibrant realm of financial technology, FinovaTech emerged as a beacon of innovation and empowerment. With a clear mission to revolutionize how individuals interacted with their finances, we strive to demystify the complexities of personal finance, making it accessible and understandable to everyone. Our aim is to empower individuals with the knowledge, tools, and resources they need to make informed financial decisions, achieve their goals, and build a secure future. With cutting-edge technology and a user-centric approach, Finovatech promises to be a trusted companion on the path to financial well-being. Together, let us unlock the true potential of your finances and embark on a journey towards financial freedom.</p>
-      </div>
-        </div>
-        
-        <div
-          className="h-[60vh] bg-center bg-cover bg-no-repeat"
+    </div><div
+            className="h-[600px] w-[600px] bg-contain bg-no-repeat "
           style={{
               backgroundImage: `url(${image2})`,
           }}
       >
-
+    <div className="text-white text-3xl  w-[360px] relative top-[100px] left-[800px]">
+            <p>
+                In the vibrant realm of financial technology,
+                FinovaTech emerged as a beacon of innovation and
+                empowerment. With a clear mission to revolutionize
+                how individuals interacted with their finances, we
+                strive to demystify the complexities of personal finance,
+                making it accessible and understandable to everyone. Our
+                aim is to empower individuals with the knowledge, tools,
+                and resources they need to make informed financial decisions,
+                achieve their goals, and build a secure future. With cutting-edge
+                technology and a user-centric approach, Finovatech promises to be a
+                trusted companion on the path to financial well-being. Together, let
+                us unlock the true potential of your finances and embark on a journey
+                towards financial freedom.
+              </p>
+      </div>
           </div>
+
+          
+        </div>
+        
+        
         
   
         
